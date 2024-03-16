ad67c84e0615cf95ad0876f6ecd69e34e6999668
metinncetinn <metinncetinn@outlook.com>
2024-03-16 19:50:36 +0200

    first commit

---
+++ src/start_instructions_sh/start_ardupilot.sh
@@ -0,0 +1,4 @@
+
+ gnome-terminal -- bash -c "cd ~/ardupilot/Tools/autotest && ./sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I0; exec bash"
+
+ gnome-terminal -- bash -c "cd ~/ardupilot/Tools/autotest && ./sim_vehicle.py -v ArduCopter -f gazebo-iris --console -I1; exec bash"
